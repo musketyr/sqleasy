8109492309b04b5d57fdeefdcf67a201e1f2c6d4
vladimir.orany <vladimir.orany@1a75cb6c-1f92-5840-d6bd-f9830b33b928>
2010-07-21 17:40:32 +0000

    removed fasade function, works only as factory for sql helper

--- eu.ebdit.sqleasy/trunk/eu.ebdit.sqleasy/src/main/java/eu/ebdit/sqleasy/SqlEasy.java
+++ eu.ebdit.sqleasy/trunk/eu.ebdit.sqleasy/src/main/java/eu/ebdit/sqleasy/SqlEasy.java
@@ -1,13 +1,6 @@
 package eu.ebdit.sqleasy;
 
-import java.sql.Connection;
-
-import javax.sql.DataSource;
-
 import eu.ebdit.sqleasy.cp.ConnectionProvider;
-import eu.ebdit.sqleasy.cp.ConnectionProviders;
-import eu.ebdit.sqleasy.handlers.ExceptionHandler;
-import eu.ebdit.sqleasy.handlers.ExceptionHandlers;
 
 
 /**
@@ -28,39 +21,5 @@ public final class SqlEasy {
 	public static SqlHelper getHelper(ConnectionProvider provider){
 		return SqlHelperImpl.getHelper(provider);
 	}
-	
-	
-	// providers
-	
-	public static ConnectionProvider forConnection(Connection conn){
-		return ConnectionProviders.forConnection(conn);
-	}
-	
-	public static ConnectionProvider forDataSource(DataSource ds){
-		return ConnectionProviders.forDataSource(ds);
-	}
-	
-	public static ConnectionProvider forDataSource(String dataSourceName){
-		return ConnectionProviders.forDataSource(dataSourceName);
-	}
-	
-	// handlers
-	
-	public static ExceptionHandler defaultHandler(){
-		return ExceptionHandlers.defaultHandler();
-	}
-	
-	public static ExceptionHandler stackTraceHandler(){
-		return ExceptionHandlers.stackTraceHandler();
-	}
-	
-	public static ExceptionHandler wrappringExceptionHandler(){
-		return ExceptionHandlers.wrappringExceptionHandler();
-	}
 
-	public static ExceptionHandler nullSafe(ExceptionHandler handler) {
-		return ExceptionHandlers.nullSafe(handler);
-	}
-	
-	// processors
 }
