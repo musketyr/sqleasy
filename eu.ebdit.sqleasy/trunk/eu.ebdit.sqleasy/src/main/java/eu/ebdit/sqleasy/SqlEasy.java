a1de0d36e95484f5191abc1fe6ad9761ca4e77f1
vladimir.orany <vladimir.orany@1a75cb6c-1f92-5840-d6bd-f9830b33b928>
2010-07-21 15:17:06 +0000

    making facade class

--- eu.ebdit.sqleasy/trunk/eu.ebdit.sqleasy/src/main/java/eu/ebdit/sqleasy/SqlEasy.java
+++ eu.ebdit.sqleasy/trunk/eu.ebdit.sqleasy/src/main/java/eu/ebdit/sqleasy/SqlEasy.java
@@ -1,6 +1,13 @@
 package eu.ebdit.sqleasy;
 
+import java.sql.Connection;
+
+import javax.sql.DataSource;
+
 import eu.ebdit.sqleasy.cp.ConnectionProvider;
+import eu.ebdit.sqleasy.cp.ConnectionProviders;
+import eu.ebdit.sqleasy.handlers.ExceptionHandler;
+import eu.ebdit.sqleasy.handlers.ExceptionHandlers;
 
 
 /**
@@ -22,4 +29,38 @@ public final class SqlEasy {
 		return SqlHelperImpl.getHelper(provider);
 	}
 	
+	
+	// providers
+	
+	public static ConnectionProvider forConnection(Connection conn){
+		return ConnectionProviders.forConnection(conn);
+	}
+	
+	public static ConnectionProvider forDataSource(DataSource ds){
+		return ConnectionProviders.forDataSource(ds);
+	}
+	
+	public static ConnectionProvider forDataSource(String dataSourceName){
+		return ConnectionProviders.forDataSource(dataSourceName);
+	}
+	
+	// handlers
+	
+	public static ExceptionHandler defaultHandler(){
+		return ExceptionHandlers.defaultHandler();
+	}
+	
+	public static ExceptionHandler stackTraceHandler(){
+		return ExceptionHandlers.stackTraceHandler();
+	}
+	
+	public static ExceptionHandler wrappringExceptionHandler(){
+		return ExceptionHandlers.wrappringExceptionHandler();
+	}
+
+	public static ExceptionHandler nullSafe(ExceptionHandler handler) {
+		return ExceptionHandlers.nullSafe(handler);
+	}
+	
+	// processors
 }
