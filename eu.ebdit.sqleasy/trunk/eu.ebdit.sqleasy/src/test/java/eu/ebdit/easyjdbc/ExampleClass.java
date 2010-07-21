e522726759f9ddeeb535b20c5f42decd340795dd
vladimir.orany <vladimir.orany@1a75cb6c-1f92-5840-d6bd-f9830b33b928>
2010-07-21 14:52:37 +0000

    addedd example class

--- eu.ebdit.sqleasy/trunk/eu.ebdit.sqleasy/src/test/java/eu/ebdit/easyjdbc/ExampleClass.java
+++ eu.ebdit.sqleasy/trunk/eu.ebdit.sqleasy/src/test/java/eu/ebdit/easyjdbc/ExampleClass.java
@@ -9,48 +9,48 @@ import eu.ebdit.sqleasy.SqlEasy;
 import eu.ebdit.sqleasy.SqlHelper;
 import eu.ebdit.sqleasy.cp.ConnectionProvider;
 import eu.ebdit.sqleasy.cp.ConnectionProviders;
+import eu.ebdit.sqleasy.handlers.ExceptionHandlers;
 
 public class ExampleClass {
 
+	public static enum MyFancyObjectProcessor implements
+			ResultProcessor<MyFancyObject> {
+		INSTANCE;
+		
+		@Override public MyFancyObject processResultSet(Iterable<ResultSet> irs)
+				throws SQLException {
+			for (ResultSet rs : irs) {
+				MyFancyObject myf = new MyFancyObject();
+				myf.setJmeno(rs.getString("JMENO"));
+				myf.setPrijmeni(rs.getString("PRIJMENI"));
+				myf.setBla(rs.getString("BLA"));
+				return myf;
+			}
+			return null;
+		}
+	}
+
 	public static class MyFancyObject {
 
-		public void setJmeno(String string) {
-			// TODO Auto-generated method stub
-			
-		}
+		public void setJmeno(String string) {}
 
-		public void setPrijmeni(String string) {
-			// TODO Auto-generated method stub
-			
-		}
+		public void setPrijmeni(String string) {}
 
-		public void setBla(String string) {
-			// TODO Auto-generated method stub
-			
-		}
+		public void setBla(String string) {}
 
 	}
 
 	public static void main(String[] args) {
+		// tohle by mohl delat supplier
 		ConnectionProvider provider = ConnectionProviders.forConnection(null);
 		SqlHelper helper = SqlEasy.getHelper(provider);
+		helper.setHandler(ExceptionHandlers.stackTraceHandler());
+		
 		String bla = helper.executeQuery("SELECT BLA FROM TABLE WHERE ID=?", 1)
 			.processWith(ResultProcessors.first(String.class));
 		
 		MyFancyObject obj = helper.executeQuery("SELECT JMENO, PRIJMENI, BLA FROM FANCY WHERE ID = ?", 5)
-			.processWith(new ResultProcessor<MyFancyObject>() {
-				@Override public MyFancyObject processResultSet(Iterable<ResultSet> irs)
-						throws SQLException {
-					for (ResultSet rs : irs) {
-						MyFancyObject myf = new MyFancyObject();
-						myf.setJmeno(rs.getString("JMENO"));
-						myf.setPrijmeni(rs.getString("PRIJMENI"));
-						myf.setBla(rs.getString("BLA"));
-						return myf;
-					}
-					return null;
-				}
-			});
+			.processWith(MyFancyObjectProcessor.INSTANCE);
 	}
 	
 }
